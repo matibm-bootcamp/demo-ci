71740c8b82f9ff92f530f9e2a562dcc58c023628
unknown <kewel2010.mb@gmail.com>
2020-06-10 11:46:30 -0400

    primer commit de ci-demo

---
+++ src/main/java/Math.java
@@ -0,0 +1,13 @@
+
+public class Math {
+
+	/***
+	 * Suma
+	 * @param x
+	 * @param y
+	 * @return entero de x + y
+	 */
+	public static int suma(int x, int y) {
+		return x + y;
+	}
+}
---
+++ src/test/java/MathTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MathTest {
+
+	@Test
+	public void testSuma() {
+		assertEquals(2, Math.suma(1,1));
+	}
+
+}
